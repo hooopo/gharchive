ad668f50920a1fa58bb3150d57241f43123c5463
Hooopo Wang <hoooopo@gmail.com>
2021-11-23 19:21:43 +0800

    if_not_exists: true

--- db/migrate/20211123100645_add_indexes.rb
+++ db/migrate/20211123100645_add_indexes.rb
@@ -1,13 +1,13 @@
 class AddIndexes < ActiveRecord::Migration[6.1]
   def change
-    add_index :github_events, :created_at
-    add_index :github_events, :actor_id
-    add_index :github_events, :actor_login
-    add_index :github_events, :repo_id
-    add_index :github_events, :repo_name
-    add_index :github_events, :language
-    add_index :github_events, :action 
-    add_index :github_events, :is_oss_db
-    add_index :github_events, :type
+    add_index :github_events, :created_at, if_not_exists: true
+    add_index :github_events, :actor_id, if_not_exists: true
+    add_index :github_events, :actor_login, if_not_exists: true
+    add_index :github_events, :repo_id, if_not_exists: true
+    add_index :github_events, :repo_name, if_not_exists: true
+    add_index :github_events, :language, if_not_exists: true
+    add_index :github_events, :action, if_not_exists: true
+    add_index :github_events, :is_oss_db, if_not_exists: true
+    add_index :github_events, :type, if_not_exists: true
   end
 end

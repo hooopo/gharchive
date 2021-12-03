dd7946ec47bb3934d57064feb64395ba11283d43
Hooopo Wang <hoooopo@gmail.com>
2021-12-03 11:25:34 +0800

    tmp rm body & bool

--- import_from_bigquery.sql
+++ import_from_bigquery.sql
@@ -1,9 +1,7 @@
-SELECT 
+CREATE TEMP TABLE archive AS SELECT 
   id,
   type,
   created_at,
-  other,
-  false as is_oss_db,
   repo.id as repo_id,
     CASE repo.name
       WHEN '/' THEN json_value(payload, '$.repo')
@@ -13,7 +11,6 @@ SELECT
   as repo_name,
   actor.id as actor_id,
   actor.login as actor_login,
-  null as actor_location,
   json_value(payload, '$.pull_request.base.repo.language') as language,
   json_value(payload, '$.pull_request.additions') as additions,
   json_value(payload, '$.pull_request.deletions') as deletions,
@@ -26,37 +23,14 @@ SELECT
   as number,
   json_value(payload, '$.commit.commit_id') as commit_id,
   json_value(payload, '$.comment.id') as comment_id,
-    CASE 
-      WHEN json_value(payload, '$.review.body') is not null THEN json_value(payload, '$.review.body')
-      WHEN json_value(payload, '$.comment.body') is not null THEN json_value(payload, '$.comment.body')
-      WHEN json_value(payload, '$.issue.body') is not null THEN json_value(payload, '$.issue.body')
-      WHEN json_value(payload, '$.pull_request.body') is not null THEN json_value(payload, '$.pull_request.body')
-      WHEN json_value(payload, '$.release.body') is not null THEN json_value(payload, '$.release.body')
-      ELSE null
-    END 
-  as body,
   org.login as org_login,
   org.id as org_id,
-   CASE type
-      WHEN 'IssuesEvent' THEN ARRAY(
-        SELECT JSON_EXTRACT_SCALAR(x, '$.name') 
-        FROM UNNEST(JSON_EXTRACT_ARRAY(payload, "$.issue.labels"))x
-      )
-      ELSE NULL
-    END
-  as labels,
     CASE 
       WHEN json_value(payload, '$.pull_request.state') is not null THEN json_value(payload, '$.pull_request.state')
       WHEN json_value(payload, '$.issue.state') is not null THEN json_value(payload, '$.issue.state')
       ELSE null
     END 
   as state,
-    CASE 
-      WHEN json_value(payload, '$.pull_request.locked') is not null THEN json_value(payload, '$.pull_request.locked')
-      WHEN json_value(payload, '$.issue.locked') is not null THEN json_value(payload, '$.issue.locked')
-      ELSE null
-    END 
-  as locked,
     CASE 
       WHEN json_value(payload, '$.pull_request.closed_at') is not null THEN json_value(payload, '$.pull_request.closed_at')
       WHEN json_value(payload, '$.issue.closed_at') is not null THEN json_value(payload, '$.issue.closed_at')
@@ -76,16 +50,8 @@ SELECT
     END
   as milestone,
   json_value(payload, '$.pull_request.merged_at') as pr_merged_at,
-  json_value(payload, '$.pull_request.draft') as pr_draft,
-  json_value(payload, '$.pull_request.merged') as pr_merged,
   json_value(payload, '$.pull_request.changed_files') as pr_changed_files,
   json_value(payload, '$.pull_request.review_comments') as pr_review_comments,
-    CASE 
-      WHEN json_value(payload, '$.pull_request.base.user.site_admin') is not null THEN json_value(payload, '$.pull_request.base.user.site_admin')
-      WHEN json_value(payload, '$.issue.user.site_admin') is not null THEN json_value(payload, '$.issue.user.site_admin')
-      ELSE null
-    END
-  as github_staff,
     CASE 
       WHEN json_value(payload, '$.pull_request.id') is not null THEN json_value(payload, '$.pull_request.id')
       WHEN json_value(payload, '$.issue.id') is not null THEN json_value(payload, '$.issue.id')
@@ -96,5 +62,12 @@ SELECT
   EXTRACT(month FROM created_at) as event_month,
   EXTRACT(year FROM created_at) as event_year
 
-FROM `githubarchive.day.20200101`
-LIMIT 50
+FROM `githubarchive.day.20200522`;
+
+EXPORT DATA OPTIONS(
+  uri='gs://gharchive.live/day1/*.csv',
+  format='CSV',
+  overwrite=true,
+  header=true,
+  field_delimiter=',') AS
+  SELECT * FROM archive

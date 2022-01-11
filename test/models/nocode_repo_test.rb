b8e9006f1770b47bb05f07360ecbec569d26f88d
Hooopo Wang <hoooopo@gmail.com>
2022-01-11 15:19:32 +0800

    sync schema

--- test/models/nocode_repo_test.rb
+++ test/models/nocode_repo_test.rb
@@ -1,3 +1,10 @@
+# == Schema Information
+#
+# Table name: nocode_repos
+#
+#  id   :string(255)      not null, primary key
+#  name :string(255)
+#
 require "test_helper"
 
 class NocodeRepoTest < ActiveSupport::TestCase

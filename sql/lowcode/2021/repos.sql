fd3d65e406ec8233285f53702379b95f78101ad8
Hooopo Wang <hoooopo@gmail.com>
2022-02-25 17:03:33 +0800

    add lowcode analysis sql

--- sql/lowcode/2021/repos.sql
+++ sql/lowcode/2021/repos.sql
@@ -3,7 +3,7 @@
          count(*) as stars, 
          any_value(jf.id) as repo_id
     FROM github_events
-         JOIN js_framework_repos jf ON jf.id = github_events.repo_id
+         JOIN nocode_repos jf ON jf.id = github_events.repo_id
    WHERE type = 'WatchEvent' 
 GROUP BY 1
 ORDER BY 2 DESC
